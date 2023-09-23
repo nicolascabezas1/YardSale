b06717c9dd393fcb18d1fb0e76b5082b896dcac5
Nicolás Cabezas <cabezasn628@gmail.com>
2023-09-22 23:34:16 -0500

    :sparkles: [New feature]  Design of the finished login screen and styles attached to the project

--- app/src/main/java/com/android/yardsale/MainActivity.kt
+++ app/src/main/java/com/android/yardsale/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.android.yardsale
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.android.yardsale.models.LoginActivity
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var imageViewYardSaleLogo: ImageView
+    private lateinit var buttonGetInto: MaterialButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initComponents()
+        setListeners()
+    }
+
+    private fun initComponents() {
+        imageViewYardSaleLogo = findViewById(R.id.imageViewYardSaleLogo)
+        buttonGetInto = findViewById(R.id.buttonGetInto)
+    }
+
+    private fun setListeners() {
+        buttonGetInto.setOnClickListener {
+            navigateToLogin()
+        }
+    }
+
+    private fun navigateToLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
