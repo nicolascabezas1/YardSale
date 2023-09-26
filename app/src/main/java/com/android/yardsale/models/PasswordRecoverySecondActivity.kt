00fda2a032909f3e661c7c94f30466e1a4e31071
Nicolás Cabezas <cabezasn628@gmail.com>
2023-09-25 19:24:22 -0500

    :sparkles: [Introduce new features second recovery password] completion of second view recover password

---
+++ app/src/main/java/com/android/yardsale/models/PasswordRecoverySecondActivity.kt
@@ -0,0 +1,12 @@
+package com.android.yardsale.models
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.yardsale.R
+
+class PasswordRecoverySecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password_recovery_second)
+    }
+}
--- app/src/main/java/com/android/yardsale/models/RecoveryPasswordActivity.kt
+++ app/src/main/java/com/android/yardsale/models/RecoveryPasswordActivity.kt
@@ -1,12 +1,17 @@
 package com.android.yardsale.models
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import com.android.yardsale.R
+import com.google.android.material.button.MaterialButton
 
 class RecoveryPasswordActivity : AppCompatActivity() {
     private lateinit var textViewBackLogin: TextView
+    private lateinit var textInputEditEmail: TextView
+    private lateinit var buttonSubmit: MaterialButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recovery_password)
@@ -15,10 +20,23 @@ class RecoveryPasswordActivity : AppCompatActivity() {
     }
     private fun initComponents() {
         textViewBackLogin = findViewById(R.id.textViewBackLogin)
+        textInputEditEmail = findViewById(R.id.textInputEditEmail)
+        buttonSubmit = findViewById(R.id.buttonSubmit)
     }
     private fun initListeners() {
         textViewBackLogin.setOnClickListener {
             onBackPressed()
         }
+        buttonSubmit.setOnClickListener {
+            if (textInputEditEmail.text.isNotEmpty()) {
+                navigateToPasswordSecond()
+            } else {
+                Toast.makeText(this, "Ingresa un email", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    private fun navigateToPasswordSecond() {
+        val intent = Intent(this, PasswordRecoverySecondActivity::class.java)
+        startActivity(intent)
     }
 }
