909e586f48669b967f1962a14562ee3a0918318e
Nicolás Cabezas <cabezasn628@gmail.com>
2023-09-25 19:53:17 -0500

    :sparkles: [Introduce new features create new password] completion of view create a new password

---
+++ app/src/main/java/com/android/yardsale/models/NewPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.android.yardsale.models
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.yardsale.R
+
+class NewPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_password)
+    }
+}
--- app/src/main/java/com/android/yardsale/models/PasswordRecoverySecondActivity.kt
+++ app/src/main/java/com/android/yardsale/models/PasswordRecoverySecondActivity.kt
@@ -1,12 +1,26 @@
 package com.android.yardsale.models
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.yardsale.R
+import com.google.android.material.button.MaterialButton
 
 class PasswordRecoverySecondActivity : AppCompatActivity() {
+    private lateinit var buttonLogin: MaterialButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_password_recovery_second)
+        initComponents()
+        initListener()
+    }
+    private fun initComponents() {
+        buttonLogin = findViewById(R.id.buttonLogin)
+    }
+    private fun initListener() {
+        buttonLogin.setOnClickListener {
+            val intent = Intent(this, NewPasswordActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
