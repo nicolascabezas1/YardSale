685518aebf1c7af7eb0c4f0a6d3ac426a77a5c39
Nicolás Cabezas <cabezasn628@gmail.com>
2023-09-29 22:06:50 -0500

    :sparkles: [New feature]:: My account activity

--- app/src/main/java/com/android/yardsale/models/LoginActivity.kt
+++ app/src/main/java/com/android/yardsale/models/LoginActivity.kt
@@ -3,9 +3,11 @@ package com.android.yardsale.models
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
 import com.android.yardsale.R
+import com.android.yardsale.models.objects.CreateAccountActivity
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
 
@@ -32,8 +34,15 @@ class LoginActivity : AppCompatActivity() {
 
     private fun initListeners() {
         textViewForgotPassword.setOnClickListener {
-            val intent = Intent(this, RecoveryPasswordActivity::class.java)
-            startActivity(intent)
+            navigateActivity(RecoveryPasswordActivity::class.java)
         }
+        buttonSecondSignUp.setOnClickListener {
+            navigateActivity(CreateAccountActivity::class.java)
+        }
+    }
+
+    private fun navigateActivity(targetActivity: Class<*>) {
+        val intent = Intent(this, targetActivity)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/android/yardsale/models/objects/CreateAccountActivity.kt
@@ -0,0 +1,12 @@
+package com.android.yardsale.models.objects
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.yardsale.R
+
+class CreateAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
